1c424dbeede2d5fe9a27e7eec08b705e0ebc2411
CapitaineJSparrow <6690149+CapitaineJSparrow@users.noreply.github.com>
2022-10-12 02:00:28 +0200

    Fix threading issue when clearing videos

--- ui/widgets/header.py
+++ ui/widgets/header.py
@@ -1,16 +1,19 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 from threading import Timer
 from ui.utils import clear_installed_videos
 
 
 class Header(Gtk.Box):
-    def reset_label(self, _, __):
+    def reset_label(self):
         self.clear_button.set_label(self.original_label)
+
+    def reset_label_idle(self):
+        GLib.idle_add(self.reset_label)
 
     def clear_videos(self, _=None):
         clear_installed_videos()
         self.clear_button.set_label("Done !")
-        Timer(3.0, self.reset_label, ("arg1", "arg2")).start()
+        Timer(3.0, self.reset_label_idle).start()
 
     def __init__(self):
         super(Header, self).__init__()

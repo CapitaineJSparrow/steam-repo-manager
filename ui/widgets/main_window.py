2d204b5aa0c51a2bc91a55d8c9dd4adab85b6928
Christopher Montesano <c.e.montesano@gmail.com>
2022-10-12 09:28:40 -0400

    hacked in a "more" button
    I'm not familiar with gtk, but I managed to rearrange some things and add a working "more" button.

--- ui/widgets/main_window.py
+++ ui/widgets/main_window.py
@@ -20,6 +20,7 @@ class MainWindow(Gtk.Window):
 
         self.set_position(Gtk.WindowPosition.CENTER)
         self.set_default_size(default_width, 680)
+        self.current_page = 1
 
         # workaround for focus of first entry
         dummy_entry = Gtk.Entry()
@@ -33,14 +34,10 @@ class MainWindow(Gtk.Window):
         box.set_valign(Gtk.Align.START)
         box.set_orientation(Gtk.Orientation.VERTICAL)
 
-        spinner = Gtk.Spinner()
-        spinner.start()
-
         head = Header()
 
         box.add(dummy_entry)
         box.add(head)
-        box.add(spinner)
 
         root_scroll = Gtk.ScrolledWindow()
         root_scroll.add(box)
@@ -48,32 +45,63 @@ class MainWindow(Gtk.Window):
         self.add(root_scroll)
         self.connect("destroy", Gtk.main_quit)
         self.show_all()
-        head.hide()
+        # head.hide()
+
+        self.item_container = Gtk.Box()
+        self.item_container.set_margin_top(GLOBAL_SPACING)
+        self.item_container.set_margin_left(GLOBAL_SPACING)
+        self.item_container.set_margin_right(GLOBAL_SPACING)
+        self.item_container.set_margin_bottom(GLOBAL_SPACING)
+        self.item_container.set_valign(Gtk.Align.START)
+        self.item_container.set_orientation(Gtk.Orientation.VERTICAL)
+        self.item_container.show()
+        box.add(self.item_container)
+
+        self.spinner = Gtk.Spinner()
+        self.spinner.set_margin_bottom(GLOBAL_SPACING)
+        box.add(self.spinner)
+
+        self.more_button = Gtk.Button(label="More")
+        self.more_button.set_margin_bottom(GLOBAL_SPACING)
+        self.more_button.connect('clicked', self.download_more)
+        box.add(self.more_button)
+
+        footer = Gtk.Label(label="<span>Made with ♥ by Captain J. Sparrow built on top of <a href='https://steamdeckrepo.com/'>Steam Deck Repo</a></span>")
+        footer.set_use_markup(True)
+        footer.show()
+        box.add(footer)
 
         # Dummy entry got focus, hide it now
         dummy_entry.destroy()
 
-        def on_videos_downloaded(videos):
-            spinner.stop()
-            head.show()
-            for i in range(ceil(len(videos) / 3)):
-                row = LibraryRow(videos[i * ROW_COUNT:(i + 1) * ROW_COUNT], box.get_allocated_width(), ROW_COUNT)
-                sep = Gtk.Box()
-                sep.set_margin_bottom(GLOBAL_SPACING)
-                box.add(row)
-                box.add(sep)
-                sep.show()
-                row.show_all()
-            footer = Gtk.Label(label="<span>Made with ♥ by Captain J. Sparrow built on top of <a href='https://steamdeckrepo.com/'>Steam Deck Repo</a></span>")
-            footer.set_use_markup(True)
-            footer.show()
-            box.add(footer)
-
-        def download_videos_async():
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            videos = loop.run_until_complete(get_videos())
-            GLib.idle_add(on_videos_downloaded, videos)
-
         # Get Videos in a new thread to prevent UI freeze
-        threading.Thread(target=download_videos_async, daemon=True).start()
+        threading.Thread(target=self.download_videos_async, daemon=True, kwargs={'page': 0}).start()
+
+    def on_videos_downloaded(self, videos):
+        self.spinner.stop()
+        # head.show()
+        for i in range(ceil(len(videos) / 3)):
+            row = LibraryRow(videos[i * ROW_COUNT:(i + 1) * ROW_COUNT],
+                             self.item_container.get_allocated_width(), ROW_COUNT)
+            sep = Gtk.Box()
+            sep.set_margin_bottom(GLOBAL_SPACING)
+            self.item_container.add(row)
+            self.item_container.add(sep)
+            sep.show()
+            row.show_all()
+        if videos:
+            self.more_button.show()
+
+    def download_videos_async(self, page: int):
+        self.spinner.start()
+        self.spinner.show()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        videos = loop.run_until_complete(get_videos(page))
+        GLib.idle_add(self.on_videos_downloaded, videos)
+
+    def download_more(self, *args, **kwargs):
+        self.more_button.hide()
+        next_page = self.current_page + 1
+        threading.Thread(target=self.download_videos_async, daemon=True, kwargs={'page': next_page}).start()
+        self.current_page = next_page
